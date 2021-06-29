82eafca553b027384cd56844ba5dbd490fb1ec3b
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-29 23:06:26 +0530

    update posts

--- src/components/PostThumb.tsx
+++ src/components/PostThumb.tsx
@@ -6,18 +6,18 @@ const PostThumb = ({ posts, result }: any) => {
     const { theme }: any = useSelector(state => state)
 
     if (result === 0) return <h2 className="text-center text-danger">No Post</h2>
-
+    posts = posts.filter((post: { url?: string }) => post?.url)
     return (
         <div className="post_thumb">
             {posts.map((post: any) => (
                 <Link key={post._id} to={`/post/${post._id}`}>
                     <div className="post_thumb_display">
-                        {post.images[0].url.match(/video/i) ? (
+                        {post?.images[0]?.url.match(/video/i) ? (
                             <video
                                 controls
-                                src={post.images[0].url}
+                                src={post?.images[0]?.url}
                                 //@ts-ignore
-                                alt={post.images[0].url}
+                                alt={post?.images[0]?.url}
                                 style={{ filter: theme ? 'invert(1)' : 'invert(0)' }}
                             />
                         ) : (
